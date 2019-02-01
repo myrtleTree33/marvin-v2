7a24dd03355dd26eaee2a7bd88223a70826e5e21
jhtong <me@joeltong.org>
2019-02-01 23:05:26 +0800

    added startup script

--- startup.sh
+++ startup.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-node dist/index.js \
--n ${NUM_NODES} \
--u ${MONGO_URI} \
--M ${MIN_INTERVAL} \
--m ${RAND_INTERVAL} \
--i ${JOB_INTERVAL_MS} \
--z ${DEFAULT_CACHE_TIME_MS} \
--x ${MIN_CACHE_TIME_MS} \
--c ${MAX_CACHE_TIME_MS} \
--v ${MAX_DIFF_TOLERANCE} 
+yarn run dev
